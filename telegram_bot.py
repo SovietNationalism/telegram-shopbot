683d0f43283913c81afca8b4eb818c84a2644e24
SovietNationalism <blasterskull@gmail.com>
2026-05-15 21:55:17 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -154,9 +154,9 @@ class ShopBot:
                         "20g 125€\n"
                         "30g 175€\n"
                         "40g 200€\n"
-                        "50g 220€\n"
-                        "100g 380€\n"
-                        "200g 720€\n"
+                        "50g 180€\n"
+                        "100g 330€\n"
+                        "200g 600€\n"
                         "Un drysift commerciale di buona qualità.\n"
                         "Fumo dall'aroma intenso e naturale, facile da lavorare e piacevole da fumare in ogni modo. Colpisce con una botta potente e duratura, lasciando un effetto rilassante e pulito."
                     ),
